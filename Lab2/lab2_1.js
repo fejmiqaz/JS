63465ab248b8f841bf9ec86aecb7cd233b4da2aa
Fejmi <fejmiqaz@gmail.com>
2024-11-02 22:13:18 +0100

    Update lab2_1.js

--- Lab2/lab2_1.js
+++ Lab2/lab2_1.js
@@ -1,3 +1,5 @@
+/* Wrote a function removeColor() that prints in the consolve the currently selected element from the list and will delete it. */
+
 function removeColor(){
     const colorSelect = document.getElementById("colorSelect")
     if(colorSelect.selectedIndex !== -1){
